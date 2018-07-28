c7a8039f387f871fe362e160eb61e6b604b82b87
asyntrail <asyntrail@gmail.com>
2018-07-28 19:42:07 +0300

    add slick slider

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,17 @@
-// import 'jquery';
+import 'jquery';
 import toggleSidebar from './lib/collapse.js';
 
 document.getElementById('toggle-btn').addEventListener('click',toggleSidebar);
 
 
+
+import 'slick-carousel';
+
+$('.slider').slick({
+  arrows: true,
+  nextArrow: '<span class="arrow-next"></span>',
+  prevArrow: '<span class="arrow-prev"></span>',
+  fade: true
+});
 
 
