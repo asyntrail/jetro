734f883acf6e0a24f53155d03cb292326b463b05
asyntrail <asyntrail@gmail.com>
2018-07-30 19:52:21 +0300

    add lightbox2 gallery

--- src/js/app.js
+++ src/js/app.js
@@ -4,14 +4,10 @@ import toggleSidebar from './lib/collapse.js';
 document.getElementById('toggle-btn').addEventListener('click',toggleSidebar);
 
 
-
 import 'slick-carousel';
+import initSlider from './lib/slick.js';
+initSlider();
 
-$('.slider').slick({
-  arrows: true,
-  nextArrow: '<span class="arrow-next"></span>',
-  prevArrow: '<span class="arrow-prev"></span>',
-  fade: true
-});
 
+import 'lightbox2';
 
---
+++ src/js/lib/slick.js
@@ -0,0 +1,11 @@
+/*eslint-env jquery*/
+function initSlider() {
+  $('.slider').slick({
+    arrows: true,
+    nextArrow: '<span class="arrow-next"></span>',
+    prevArrow: '<span class="arrow-prev"></span>',
+    fade: true
+  });
+}
+
+module.exports = initSlider;
